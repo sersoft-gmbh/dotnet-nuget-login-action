c4a815dd72a23d9ffc7c1e8e987a5bc3c99c3f31
Florian Friedrich <florian.friedrich@sersoft.de>
2026-04-22 10:20:27 +0200

    Allow updating existing sources

--- README.md
+++ README.md
@@ -6,34 +6,48 @@ A GitHub action that logs into a NuGet registry.
 
 ## Inputs
 
-### `registry-url`
+### `update`
 
-The URL of the NuGet registry to log in to.
+Whether to update an existing registry instead of adding a new one. Default is `false`.
+If set to `true`, the `nuget sources update` command is used. Otherwise `nuget sources add` is used. 
+Note that the action will set the credentials to empty values in the post-step (instead of removing the source) when `update` is `true.
 
 ### `registry-name`
 
 The name to use for the registry.
-Equivalent to the `--name` parameter of the `nuget sources add` command.
+Equivalent to the `--name` parameter of the `nuget sources add` command or the first positional argument of `nuget sources update`.
+
+### `registry-url`
+
+The URL of the NuGet registry to log in to. Not required if `update` is used.
+If `update` is `true`, this is the passed as the `--source` parameter of the `nuget sources update` command.
+
+### `protocol-version`
+
+The protocol version to use for the registry.
+Equivalent to the `--protocol-version` parameter of the `nuget sources add` / `nuget sources update` command.
 
 ### `username`
 
 The username to use for the login. Use a secret to avoid exposing it in the logs.
-Equivalent to the `--username` parameter of the `nuget sources add` command.
+You can also use an environment variable placeholder (e.g. `%NUGET_USERNAME%`) and provide it as environment variable (e.g. `NUGET_USERNAME`) in the rest of the workflow.
+Equivalent to the `--username` parameter of the `nuget sources add` / `nuget sources update` command.
 
 ### `password`
 
 The password to use for the login. Use a secret to avoid exposing it in the logs.
-Equivalent to the `--password` parameter of the `nuget sources add` command.
+You can also use an environment variable placeholder (e.g. `%NUGET_PASSWORD%`) and provide it as environment variable (e.g. `NUGET_PASSWORD`) in the rest of the workflow.
+Equivalent to the `--password` parameter of the `nuget sources add` / `nuget sources update` command.
 
 ### `store-password-in-cleartext`
 
-Whether to store the password in cleartext. The cleanup of this action will remove it again.
-Equivalent to the `--store-password-in-clear-text` parameter of the `nuget sources add` command.
+Whether to store the password in cleartext. The cleanup of this action will remove / replace it again.
+Equivalent to the `--store-password-in-clear-text` parameter of the `nuget sources add` / `nuget sources update` command.
 
 ### `config-file`
 
 The path to the NuGet configuration file. Uses the default config file by default.
-Equivalent to the `--configfile` parameter of the `nuget sources add` command.
+Equivalent to the `--configfile` parameter of the `nuget sources add` / `nuget sources update` command.
 
 
 ## Example Usage
@@ -41,8 +55,10 @@ Equivalent to the `--configfile` parameter of the `nuget sources add` command.
 ```yaml
 uses: sersoft-gmbh/dotnet-nuget-login-action@v2
 with:
-    registry-url: 'https://nuget.example.com/v3/index.json'
+    update: false
     registry-name: example
+    registry-url: 'https://nuget.example.com/v3/index.json'
+    protocol-version: 3
     username: ${{ secrets.NUGET_USERNAME }}
     password: ${{ secrets.NUGET_PASSWORD }}
 ```
--- src/main.ts
+++ src/main.ts
@@ -2,55 +2,73 @@ import * as core from "@actions/core";
 import {exec} from "@actions/exec";
 
 async function runCmd(cmd: string, ...args: string[]): Promise<void> {
-    await exec(cmd, args.length <= 0 ? undefined : args, {silent: !core.isDebug()});
+  await exec(cmd, args.length <= 0 ? undefined : args, {silent: !core.isDebug()});
 }
 
 async function main() {
-    core.startGroup('Validate input');
-    const registryUrl = core.getInput('registry-url', {required: true});
-    const registryName = core.getInput('registry-name', {required: true});
-    const username = core.getInput('username');
-    const password = core.getInput('password', {required: !!username});
-    const storePasswordInCleartext = core.getBooleanInput('store-password-in-cleartext',
-        {required: !!password});
-    const configFilePath = core.getInput('config-file');
-    if (!username && password) throw new Error('Password cannot be set without username');
-    core.endGroup();
+  core.startGroup('Validate input');
+  const update = core.getBooleanInput('update', {required: true});
+  const registryName = core.getInput('registry-name', {required: true});
+  const registryUrl = core.getInput('registry-url', {required: !update});
+  const protocolVersion = core.getInput('protocol-version');
+  const username = core.getInput('username');
+  const password = core.getInput('password', {required: !!username});
+  const storePasswordInCleartext = core.getBooleanInput('store-password-in-cleartext',
+    {required: !!password});
+  const configFilePath = core.getInput('config-file');
+  if (!username && password) throw new Error('Password cannot be set without username');
+  core.endGroup();
 
-    await core.group('Add registry', async () => {
-        core.saveState('registryName', registryName);
-        let args = [
-            'nuget',
-            'add',
-            'source',
-            registryUrl,
-            '--name',
-            registryName,
-        ]
-        if (username) args.push('--username', username);
-        if (password) {
-            args.push('--password', password);
-            if (storePasswordInCleartext) args.push('--store-password-in-clear-text');
-        }
-        if (configFilePath) // noinspection SpellCheckingInspection
-            args.push('--configfile', configFilePath);
-        await runCmd('dotnet', ...args);
-    });
+  core.saveState('registryName', registryName);
+  core.saveState('update', update ? 'true' : 'false');
+  core.saveState('postStepNeeded', !update || (username || password) ? 'true' : 'false');
+  if (configFilePath) core.saveState('configFilePath', configFilePath);
+
+  await core.group(`${update ? 'Update' : 'Add'} registry`, async () => {
+    let args = ['nuget'];
+    if (update) {
+      args.push('update', 'source', registryName);
+      if (registryUrl) args.push('--source', registryUrl);
+    } else {
+      args.push('add', 'source', registryUrl, '--name', registryName);
+    }
+    if (username) args.push('--username', username);
+    if (password) {
+      args.push('--password', password);
+      if (storePasswordInCleartext) args.push('--store-password-in-clear-text');
+    }
+    if (protocolVersion) args.push('--protocol-version', protocolVersion);
+    if (configFilePath) // noinspection SpellCheckingInspection
+      args.push('--configfile', configFilePath);
+    await runCmd('dotnet', ...args);
+  });
 }
 
 async function post() {
-    await core.group('Remove registry', async () => {
-        const registryName = core.getState('registryName');
-        if (registryName)
-            await runCmd('dotnet', 'nuget', 'remove', 'source', registryName);
-    });
+  const postStepNeeded = core.getState('postStepNeeded') === 'true';
+  if (!postStepNeeded) return;
+  const update = core.getState('update') === 'true';
+  await core.group(`Remove registry${update ? ' credentials' : ''}`, async () => {
+    const registryName = core.getState('registryName');
+    if (registryName) {
+      const configFilePath = core.getState('configFilePath');
+      let args = ['nuget'];
+      if (update)
+        args.push('update', 'source', registryName, '--store-password-in-clear-text', '--username', '', '--password', '');
+      else
+        args.push('remove', 'source', registryName);
+      if (configFilePath) // noinspection SpellCheckingInspection
+        args.push('--configfile', configFilePath);
+      await runCmd('dotnet', ...args);
+    }
+  });
 }
 
 try {
-    const isPost = !!core.getState('isPost');
-    // Mark the next run as post
-    if (!isPost) core.saveState('isPost', 'true');
-    (isPost ? post() : main()).catch((error) => core.setFailed(error.message));
+  const isPost = !!core.getState('isPost');
+  // Mark the next run as post
+  if (!isPost) core.saveState('isPost', 'true');
+  (isPost ? post() : main()).catch((error) => core.setFailed(error.message));
 } catch (error: any) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
